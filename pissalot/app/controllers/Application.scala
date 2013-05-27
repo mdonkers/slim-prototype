a4208df5dd9e7ea2b566f44bceca882f09fd0d61
Miel Donkers <miel.donkers@codecentric.nl>
2013-05-27 20:28:54 +0200

    Moved parsing (writing and now also reading) of JSON data to the User class

--- pissalot/app/controllers/Application.scala
+++ pissalot/app/controllers/Application.scala
@@ -2,7 +2,7 @@ package controllers
 
 import play.api.mvc._
 import models.User
-import play.api.libs.json.{JsValue, Writes, Json}
+import play.api.libs.json.Json
 import play.api.libs.json.Json._
 
 object Application extends Controller {
@@ -12,16 +12,6 @@ object Application extends Controller {
   }
 
   def users = Action {
-
-    // Defines JSON writer for the User class
-    implicit val creatureWrites = new Writes[User] {
-      def writes(c: User): JsValue = {
-        Json.obj(
-          "name" -> c.name,
-          "email" -> c.email
-        )
-      }
-    }
 
     val miel = User(1, "Miel", "miel.donkers@codecentric.nl")
     val hylke = User(2, "Hylke", "hylke.stapersma@codecentric.nl")
--- pissalot/app/models/User.scala
+++ pissalot/app/models/User.scala
@@ -1,6 +1,27 @@
 package models
 
+import play.api.libs.json.{Format, JsValue, Json, JsSuccess}
+
 /**
  * TODO update header from template
  */
 case class User(id: Long, name: String, email: String)
+
+object User {
+
+  implicit object LeagueFormat extends Format[User] {
+    def reads(json: JsValue): JsSuccess[User] = JsSuccess(User(
+      (json \ "id").as[Long],
+      (json \ "name").as[String],
+      (json \ "email").as[String]
+    ))
+
+    def writes(u: User): JsValue = {
+      Json.obj(
+        "name" -> u.name,
+        "email" -> u.email
+      )
+    }
+  }
+
+}
