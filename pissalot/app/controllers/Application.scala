26a030cf20382ce8573137890a29596110844969
Miel Donkers <miel.donkers@codecentric.nl>
2013-05-27 17:41:33 +0200

    Added action 'users' and corresponding class, converting succesfully to JSON data

--- pissalot/app/controllers/Application.scala
+++ pissalot/app/controllers/Application.scala
@@ -1,12 +1,41 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import models.User
+import play.api.libs.json.{JsValue, Writes, Json}
+import play.api.libs.json.Json._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  def users = Action {
+
+    // Defines JSON writer for the User class
+    implicit val creatureWrites = new Writes[User] {
+      def writes(c: User): JsValue = {
+        Json.obj(
+          "name" -> c.name,
+          "email" -> c.email
+        )
+      }
+    }
+
+    val miel = User(1, "Miel", "miel.donkers@codecentric.nl")
+    val hylke = User(2, "Hylke", "hylke.stapersma@codecentric.nl")
+
+    val jsonArray = Json.toJson(Seq(
+      toJson(miel), toJson(hylke)
+    ))
+
+    Ok(Json.toJson(jsonArray))
+  }
+
+
+  def newUser = TODO
+
+  def deleteUser(id: Long) = TODO
+
 }
---
+++ pissalot/app/models/User.scala
@@ -0,0 +1,6 @@
+package models
+
+/**
+ * TODO update header from template
+ */
+case class User(id: Long, name: String, email: String)
