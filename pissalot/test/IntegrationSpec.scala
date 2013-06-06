513ea7fe4e87ded3b9d6df6c4c1a0eb15649ba41
Miel Donkers <miel.donkers@codecentric.nl>
2013-06-06 11:46:14 +0200

    Disabled JavaScript for HTMLUnit browser to make tests pass (test would hang forever)

--- pissalot/test/IntegrationSpec.scala
+++ pissalot/test/IntegrationSpec.scala
@@ -4,27 +4,33 @@ import org.specs2.mutable._
 
 import play.api.test._
 import play.api.test.Helpers._
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
 
 /**
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
 class IntegrationSpec extends Specification {
-  
+
   "Application" should {
-    
+
     "work from within a browser" in {
-      running(TestServer(9001), HTMLUNIT) { browser =>
+      running(TestServer(9001), HTMLUNIT) {
+        browser =>
 
-        browser.goTo("http://localhost:9001/")
+          browser.webDriver match {
+            case htmlunit: HtmlUnitDriver => htmlunit.setJavascriptEnabled(false)
+          }
 
-        browser.pageSource must contain("Pissalot Application")
+          browser.goTo("http://localhost:9001/")
 
-        browser.pageSource must contain("angular.min.js")
-       
+          browser.pageSource must contain("Pissalot Application")
+
+          browser.pageSource must contain("angular.min.js")
+
       }
     }
-    
+
   }
-  
+
 }
